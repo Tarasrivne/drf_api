5847364bb4936deefeff6e2eed1c0e2fd5b19969
Tarasrivne <TarasViktorchuk@gmail.com>
2024-05-15 15:18:55 -0400

    now all working correct

--- BooklistAPI/views.py
+++ BooklistAPI/views.py
@@ -1,14 +1,14 @@
-from django.shortcuts import render
+
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import  api_view
-
+from django.shortcuts import render
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework import generics
 
 class BookView(generics.ListCreateAPIView):
-    queryset = Book.objects.all
+    queryset = Book.objects.all()
     serializer_class = BookSerializer
 
 class SingleBookView(generics.RetrieveUpdateAPIView):
